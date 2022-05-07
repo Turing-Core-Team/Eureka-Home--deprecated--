7c144be5b1c5212ab3057a3316f800a63341fe53
Julián Camilo Velandia <jvelandiag@unal.edu.co>
2022-05-06 21:31:15 -0500

    platform structure

--- main.py
+++ main.py
@@ -1,16 +1,38 @@
-# This is a sample Python script.
+from _future_ import print_function
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+from google.oauth2 import service_account
+
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+SERVICE_ACCOUNT_FILE = 'keys.json'
+SAMPLE_RANGE_NAME = 'tags!A1:A5'
+
+creds = None
+creds = service_account.Credentials.from_service_account_file(
+    SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+
+# The ID and range of a sample spreadsheet.
+SAMPLE_SPREADSHEET_ID = '118oriZebWeyFuUlzQjJuLiEDmE2CVHpIhmpglw51N40'
+
+def main():
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    try:
+        service = build('sheets', 'v4', credentials=creds)
+
+        # Call the Sheets API
+        sheet = service.spreadsheets()
+        result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                                    range=SAMPLE_RANGE_NAME).execute()
+        '''values = result.get('values', [])'''
+
+        print(result)
+
+    except HttpError as err:
+        print(err)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == "__main__":
+    main()
